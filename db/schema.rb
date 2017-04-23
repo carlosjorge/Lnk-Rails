231bce7215b237017248b42b54f8e89ec7377638
carlosjorge <carlosjorge15@hotmail.com>
2017-04-23 11:07:15 -0300

    Creating Models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423033500) do
+ActiveRecord::Schema.define(version: 20170423133941) do
+
+  create_table "links", force: :cascade do |t|
+    t.text     "url"
+    t.string   "short"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
